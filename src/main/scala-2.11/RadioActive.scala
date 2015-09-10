4792d7bf5477e53e09749a02a9f8722c56b4c016
kharole <oleg.kharchenko@gmail.com>
2015-09-10 10:14:24 +0300

    performance optimization: BitSets and test plan building

--- src/main/scala-2.11/RadioActive.scala
+++ src/main/scala-2.11/RadioActive.scala
@@ -1,26 +1,33 @@
+import scala.collection.BitSet
+
 object RadioActive {
 
-  def combinations(l: List[Int], k: Int): Stream[List[Int]] =
-    if (k == l.length)
-      Stream(l)
+  object |<| {
+    def unapply(s: BitSet): Option[(Int, BitSet)] =
+      if (s.isEmpty) None
+      else Some((s.head, s.tail))
+  }
+
+  def combinations(set: BitSet, k: Int): Stream[BitSet] =
+    if (k == set.size)
+      Stream(set)
     else
-      l match {
-        case x :: xs => combinations(xs, k - 1).map(c => x :: c) ++ combinations(xs, k)
-        case Nil => Stream()
+      set match {
+        case head |<| tail => combinations(tail, k - 1).map(_ + head) ++ combinations(tail, k)
+        case _ => Stream()
       }
 
-  def combinationsNumber(n: Int, k: Int): Int = combinations((1 to n).toList, k).length
-
-  def solvable(combinations: List[List[Int]], attempts: Int): Boolean =
+  def solvable(combinations: Set[BitSet], attempts: Int): Boolean =
     solve(combinations, attempts).exists(_.isDefined)
 
-  def solve(combinations: List[List[Int]], attempts: Int): Stream[Option[Tree[List[Int]]]] = combinations match {
-    case head :: Nil => Stream(Some(Leaf(head)))
-    case head :: tail => if (attempts == 0)
+  def solve(combinations: Set[BitSet], attempts: Int): Stream[Option[Tree[BitSet]]] =
+    if (combinations.size == 1)
+      Stream(Some(Leaf(combinations.head)))
+    else if (attempts == 0 || combinations.isEmpty)
       Stream(None)
-    else {
+    else
       for {
-        test <- testPlan(combinations, attempts).toStream
+        test <- testPlan(combinations, attempts - 1).toStream
         negativeTreeOption <- solve(test._2, attempts - 1)
         positiveTreeOption <- solve(test._3, attempts - 1)
       } yield {
@@ -29,27 +36,17 @@ object RadioActive {
           positiveTree <- positiveTreeOption
         } yield Node(test._1, negativeTree, positiveTree)
       }
-    }
+
+  def beep(combinations: Set[BitSet], test: BitSet): (Set[BitSet], Set[BitSet]) =
+    combinations.partition(s => (s & test).isEmpty)
+
+  def testPlan(combinations: Set[BitSet], attempts: Int): Vector[(BitSet, Set[BitSet], Set[BitSet])] = {
+    (for {
+      test <- (BitSet.empty ++ combinations.flatMap(_.toSet)).subsets()
+      testResults = beep(combinations, test)
+      if (feasible(testResults._1, attempts) && feasible(testResults._2, attempts))
+    } yield (test, testResults._1, testResults._2)).toVector.sortBy(t => -Math.abs(t._2.size - t._3.size))
   }
 
-  def beep(possibleCombinations: List[List[Int]], test: List[Int]): List[List[Int]] =
-    (for {
-      t <- test
-      beepCombinations <- possibleCombinations.filter(c => c.contains(t))
-    } yield beepCombinations).distinct
-
-  def powerSet(balls: List[Int]): Iterable[List[Int]] = for {
-    len <- 1 to balls.length
-    test <- balls.combinations(len)
-  } yield test
-
-  def testPlan(combinations: List[List[Int]], attempts: Int): List[(List[Int], List[List[Int]], List[List[Int]])] =
-    for {
-      test <- powerSet(combinations.flatten.distinct).toList
-      positive = beep(combinations, test)
-      negative = combinations diff positive
-      if (feasible(negative, attempts) && feasible(positive, attempts))
-    } yield (test, negative, positive)
-
-  def feasible(combinations: List[List[Int]], attempts: Int) = combinations.length <= (1 << attempts)
+  def feasible[A](combinations: Set[A], attempts: Int) = combinations.size <= (1 << attempts)
 }
--- src/test/scala-2.11/RadioActive$Test.scala
+++ src/test/scala-2.11/RadioActive$Test.scala
@@ -1,24 +1,17 @@
-import RadioActive.combinations
+import scala.collection.BitSet
 
 class RadioActive$Test extends org.scalatest.FunSuite {
 
-  val balls = (1 to 15).toList
-  val c2_15 = combinations(balls, 2).toList
-
-  test("test order") {
-    println(RadioActive.powerSet((1 to 3).toList).toList)
+  def puzzle(radioActiveBalls: Int, balls: Int): Set[BitSet] = {
+    (1 to balls).toList.combinations(radioActiveBalls).map(l => BitSet.empty ++ l).toSet
   }
 
   test("C(15,2) = 105") {
-    assert(c2_15.length == 105)
+    assert(puzzle(2, 15).size == 105)
   }
 
   test("C(4,2) = 6") {
-    assert(combinations(List(1, 2, 3), 2).length == 3)
-  }
-
-  test("test1") {
-    assert(RadioActive.beep(c2_15, List(2, 7)).length == 27)
+    assert(RadioActive.combinations(BitSet(1, 2, 3), 2).length == 3)
   }
 
   test("test1 1 << 3 = 8") {
@@ -26,44 +19,54 @@ class RadioActive$Test extends org.scalatest.FunSuite {
   }
 
   test("no way to check 105 combination with 2^6 tests") {
-    assert(RadioActive.solvable(c2_15, 6))
+    assert(RadioActive.solvable(puzzle(2, 15), 6))
   }
 
   test("2 radioactive among 15 is solvable with 7 tests") {
-    val solutions = RadioActive.solve(combinations((1 to 15).toList, 2).toList, 7)
+    val solutions = RadioActive.solve(puzzle(2, 15), 7)
     println(solutions.filter(_.isDefined).head.get)
   }
 
   test("2 radioactive among 11 is not solvable with 6 tests") {
-    assert(!RadioActive.solvable(combinations((1 to 11).toList, 2).toList, 6))
+    assert(!RadioActive.solvable(puzzle(2, 11), 6))
   }
 
   test("couple of them") {
-    assert(RadioActive.solvable(combinations((1 to 6).toList, 2).toList, 4))
+    assert(RadioActive.solvable(puzzle(2, 6), 4))
   }
 
   test("simple") {
-    assert(RadioActive.solvable(combinations((1 to 1).toList, 1).toList, 0))
-    assert(RadioActive.solvable(combinations((1 to 1).toList, 1).toList, 1))
-    assert(RadioActive.solvable(combinations((1 to 2).toList, 1).toList, 1))
-    assert(RadioActive.solvable(combinations((1 to 4).toList, 1).toList, 3))
-    assert(RadioActive.solvable(combinations((1 to 4).toList, 1).toList, 2))
-    assert(!RadioActive.solvable(combinations((1 to 5).toList, 1).toList, 2))
+    assert(!RadioActive.solvable(Set(), 100))
+    assert(RadioActive.solvable(puzzle(1, 1), 0))
+    assert(RadioActive.solvable(puzzle(1, 1), 1))
+    assert(RadioActive.solvable(puzzle(1, 2), 1))
+    assert(RadioActive.solvable(puzzle(1, 4), 3))
+    assert(RadioActive.solvable(puzzle(1, 4), 2))
+    assert(!RadioActive.solvable(puzzle(1, 5), 2))
   }
 
   test("println") {
-    println(RadioActive.solve(combinations((1 to 4).toList, 1).toList, 3).filter(_.isDefined).head.get)
-    println(RadioActive.solve(combinations((1 to 100).toList, 100).toList, 0).filter(_.isDefined).head.get)
-    println(RadioActive.solve(combinations((1 to 5).toList, 2).toList, 4).filter(_.isDefined).head.get)
+    println(RadioActive.solve(puzzle(1, 3), 3).filter(_.isDefined).head.get)
+    println(RadioActive.solve(puzzle(1, 100), 0).filter(_.isDefined).head.get)
+    println(RadioActive.solve(puzzle(1, 5), 4).filter(_.isDefined).head.get)
   }
 
   test("feasible") {
-    assert(RadioActive.feasible(List(List(1), List(2), List(3), List(4)), 2))
-    assert(!RadioActive.feasible(List(List(1), List(2), List(3), List(4), List(5)), 2))
+    assert(RadioActive.feasible(Set(List(1), List(2), List(3), List(4)), 2))
+    assert(!RadioActive.feasible(Set(List(1), List(2), List(3), List(4), List(5)), 2))
+  }
+
+  test("beep") {
+    assert(RadioActive.beep(puzzle(2, 15), BitSet(2, 7))._1.size == 105 - 27) //negative
+    assert(RadioActive.beep(puzzle(2, 15), BitSet(2, 7))._2.size == 27) // positive
+
+    assert(RadioActive.beep(Set(BitSet(1), BitSet(2), BitSet(3)), BitSet())._1.size == 3)
+    assert(RadioActive.beep(Set(BitSet(1), BitSet(2), BitSet(3)), BitSet())._2.size == 0)
   }
 
   test("testPlan") {
-    println(RadioActive.testPlan((1 to 15).toList.combinations(2).toList, 7))
+    assert(RadioActive.testPlan(puzzle(2, 15), 6).size == 4368)
+    assert(RadioActive.testPlan(puzzle(2, 15), 6).toList.head._1.size == 4)
   }
 
 }
